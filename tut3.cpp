8e8582ba006779c9836a49789ef8ca54a9edf2aa
Prince Raj <princeraj18023@gmail.com>
2026-06-02 14:12:05 +0530

    Day 1: Added Tut1 and tut3

---
+++ tut3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//This program was created by Prince Raj
+/* this 
+is 
+a
+multi
+line
+comment */
+int main(){
+    int sum = 6;
+    cout<< "Hello world"<< sum;
+    return 0;
+}
