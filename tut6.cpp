bb3fae783dfa046e193117b5fc44536cb7a9ec24
Prince Raj <princeraj18023@gmail.com>
2026-06-05 12:12:34 +0530

    Day 4: tut6.cpp

---
+++ tut6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+    int main(){
+    // Assignment operators
+      int a=3, b=9;
+      char d= 'd';
+
+    //logical operator operators
+    cout<<"The value of a == b is "<<((a==b) && (a<b))<<endl;
+
+    return 0;
+
+
+}
