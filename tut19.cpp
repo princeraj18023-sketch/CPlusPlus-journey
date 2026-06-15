357eebbd5136670e4308ecd3c2c9d9d5cc460388
Prince Raj <princeraj18023@gmail.com>
2026-06-15 11:27:27 +0530

    Day 13 : tut19.cpp

---
+++ tut19.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int factorial(int n)
+{
+    if (n < 2)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+int fib(int n)
+{
+    if (n < 2)
+    {
+        return 1;
+    }
+    return fib(n - 2) + fib(n - 1);
+}
+
+int main()
+{
+    int a;
+    cout << "Enter the number" << endl;
+    cin >> a;
+    // cout<<"The factorial of "<<a<<" is  "<<factorial(a)<<endl;
+    cout << "The term in fibonacci sequence at position " << a << " is " << fib(a) << endl;
+    return 0;
+}
