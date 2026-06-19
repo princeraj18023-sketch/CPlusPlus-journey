538d8d32c408dfa9886e73117958244308d98756
Prince Raj <princeraj18023@gmail.com>
2026-06-19 11:20:51 +0530

    Day 17: tut29.cpp

---
+++ tut29.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    int a, b;
+
+public:
+    Complex(void);
+    void printNumber()
+    {
+        cout << "Your number is " << a << "+" << b << "i" << endl;
+    }
+};
+
+Complex ::Complex(void)
+{
+    a = 10;
+    b = 20;
+    cout << " Hello World " << endl;
+}
+int main()
+{
+    Complex c1, c2, c3;
+    c1.printNumber();
+    c2.printNumber();
+    c3.printNumber();
+    return 0;
+}
