35c03b8e914b8290af00bd16128c411fef33d51c
Prince Raj <princeraj18023@gmail.com>
2026-06-21 12:57:13 +0530

    Day 19 : tut31.cpp

---
+++ tut32.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Simple
+{
+    int data1;
+    int data2;
+    int data3;
+
+public:
+    Simple(int a, int b = 8, int c = 6)
+    {
+        data1 = a;
+        data2 = b;
+        data3 = c;
+    }
+    void printData();
+};
+
+void Simple ::printData()
+{
+    cout << "The value of data1, data2 and data3 is " << data1 << " , " << data2 << " and " << data3 << endl;
+}
+int main()
+{
+    Simple s(1);
+    s.printData();
+
+    return 0;
+}
